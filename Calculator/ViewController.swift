18cafdd920a7ef470a161f9315d824c2313a6a06
zhuwenjie <229657322@qq.com>
2023-06-19 17:05:53 +0800

    第一节课内容完成

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var display: UILabel!
+    
+    // 用户正在输入中
+    var userIsInTheMiddOfTyping: Bool = false
+
+    // 数字按键 触摸事件
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = (sender.titleLabel?.text)!
+        
+        if (userIsInTheMiddOfTyping) {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }
+        else {
+            display.text = digit
+            userIsInTheMiddOfTyping = true
+        }
     }
 
+    // 计算属性：显示内容的浮点值
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            // newValue 为set方法默认的新值
+            display.text = String(newValue)
+        }
+    }
 
+    // 运算符 触摸事件
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddOfTyping = false
+        if let mathmaticalSymbol = sender.titleLabel?.text {
+            switch mathmaticalSymbol {
+            case "π":
+                // display.text = "\(Double.pi)"
+                // display.text = String(Double.pi)
+                displayValue = Double.pi
+            case "√":
+                // let operand = Double(display.text!)!
+                // display.text = String(sqrt(operand))
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+    
 }
 
