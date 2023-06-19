73e2d68ab88c02991753d4a1674369fc2a96dd76
zhuwenjie <229657322@qq.com>
2023-06-19 22:34:50 +0800

    使用枚举以将运算过程抽象至操作字典（常量 和 一元运算）

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -13,15 +13,26 @@ struct CalculatorBrain {
     // 累加器
     private var accumulator: Double?
     
+    private var operations: Dictionary<String, Operation> = [
+        "π": .constant(Double.pi),
+        "e": .constant(M_E),
+        "√": .unaryOperation(sqrt),
+        "cos": .unaryOperation(cos),
+        "±": .unaryOperation(changeSign)
+    ]
+    
     // 执行计算
     mutating func performOperation(_ symbol: String) {
-        switch symbol {
-        case "π":
-            accumulator = Double.pi
-        case "√":
-            accumulator = sqrt(accumulator!)
-        default:
-            break
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
+            }
+        
         }
     }
     
@@ -36,4 +47,19 @@ struct CalculatorBrain {
         }
     }
     
+    
+    /// 运算枚举
+    private enum Operation {
+        // 常量
+        case constant(Double)
+        // 一元运算符
+        case unaryOperation((Double) -> Double)
+    }
+}
+
+/// 值取反
+/// - Parameter operand: 操作数
+/// - Returns: 对操作数取反后的值
+private func changeSign(operand: Double) -> Double {
+    return -operand
 }
